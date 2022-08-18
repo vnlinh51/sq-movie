46115ee3b580bb1c4a1a8b7dbfd3a03c447e6be9
Vu Ngoc Linh <vnlinh51@gmail.com>
2022-08-18 18:09:16 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 // Import Router
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
 
-import Header from './component/header/Header';
 import Home from './pages/Home';
+import Header from './component/header/Header';
 import Footer from './component/footer/Footer';
 import NotFund from './pages/NotFund';
-import PageHeader from './component/page-header/PageHeader';
 import Catalog from './pages/Catalog';
+import Detail from './pages/detail/Detail';
 
 function App() {
   return (
@@ -25,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/:category" element={<Catalog />} />
         <Route path="/:category/search/:keyword" element={<Catalog />} />
+        <Route path="/:category/:id" element={<Detail />} />
         <Route path="*" element={<NotFund />} />
       </Routes>
       <Footer />
