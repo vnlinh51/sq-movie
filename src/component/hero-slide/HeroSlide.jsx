0e4d43fe89555c4616d70b1b6730888f2e6f70f8
Vu Ngoc Linh <vnlinh51@gmail.com>
2022-08-18 18:21:53 +0700

    fix watch trailer btn

--- src/component/hero-slide/HeroSlide.jsx
+++ src/component/hero-slide/HeroSlide.jsx
@@ -90,8 +90,8 @@ const HeroSlideItem = (props) => {
           <h2 className="title">{item.title}</h2>
           <div className="overview">{item.overview}</div>
           <div className="btns">
-            <Button onclick={() => navigate('/movie' + item.id)}>Watch now</Button>
-            <OutlineButton onclick={setModalActive}>Watch trailer</OutlineButton>
+            <Button onClick={() => navigate('/movie/' + item.id)}>Watch now</Button>
+            <OutlineButton onClick={setModalActive}>Watch trailer</OutlineButton>
           </div>
         </div>
 
